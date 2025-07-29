840423c0591efd11013bd3655ac486eb5e49eb11
Dimetry537 <maksakov1988@gmail.com>
2025-07-29 09:53:09 +0300

    add text labels at homeframe

--- src/gui/frames/home_frame.py
+++ src/gui/frames/home_frame.py
@@ -3,8 +3,36 @@ from tkinter import ttk
 class HomeFrame(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent, padding="20")
-        ttk.Label(self, text="Welcome to the Home Frame!").grid(column=0, row=0)
-        ttk.Button(self, text="Click Me", command=self.quit_app).grid(column=1, row=0)
+
+        labels = [
+            "№",
+            "Дата",
+            "ФИО пациента",
+            "Дата рождения",
+            "Адрес",
+            "Диагноз",
+            "МКБ-10",
+            "Код ЦАХ",
+            "Врач",
+            "Мед. сестра",
+            "Когда закончен"
+        ]
+        self.entries = {}
+
+        for i, label_text in enumerate(labels):
+            label = ttk.Label(self, text=label_text)
+            label.grid(row=i, column=0, sticky="e", padx=5, pady=5)
+
+            entry = ttk.Entry(self, width=40)
+            entry.grid(row=i, column=1, padx=5, pady=5, sticky="w")
+
+            self.entries[label_text] = entry
+
+        ttk.Button(self, text="Сохранить", command=self.on_save).grid(row=len(labels), column=0, pady=10)
+        ttk.Button(self, text="Выход", command=self.quit_app).grid(row=len(labels) + 1, column=1, pady=10)
+    
+    def on_save(self):
+        pass
 
     def quit_app(self):
         self.winfo_toplevel().destroy()
