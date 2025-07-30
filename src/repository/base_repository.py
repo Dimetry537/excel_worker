ae8d9a10ccaeb88738760757a4ed533477324b7b
Dimetry537 <maksakov1988@gmail.com>
2025-07-30 16:33:25 +0300

    add base repositiry and pydantic for personal

--- src/repository/base_repository.py
+++ src/repository/base_repository.py
@@ -1,6 +1,38 @@
+from typing import Type, TypeVar, Generic
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
-class BaseRepository:
-    def __init__(self, session: AsyncSession):
+T = TypeVar('T')
+
+class BaseRepository(Generic[T]):
+    def __init__(self, session: AsyncSession, model: Type[T]):
         self.session = session
+        self.model = model
+
+    async def get_by_id(self, id: int) -> T | None:
+        stmt = select(self.model).where(self.model.id == id)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
     
+    async def get_all(self) -> list[T]:
+        stmt = select(self.model)
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
+    
+    async def create(self, obj_in: dict) -> T:
+        obj = self.model(**obj_in)
+        self.session.add(obj)
+        await self.session.commit()
+        await self.session.refresh(obj)
+        return obj
+    
+    async def update(self, obj_id: int, obj_in: dict) -> T | None:
+        obj = await self.get_by_id(obj_id)
+        if not obj:
+            return None
+        for field, value in obj_in.items():
+            setattr(obj, field, value)
+
+        await self.session.commit()
+        await self.session.refresh(obj)
+        return obj
