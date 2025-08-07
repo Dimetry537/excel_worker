541538b4090af9583b86cf114262da694cbc3379
Dimetry537 <maksakov1988@gmail.com>
2025-08-07 13:30:57 +0300

    add apirouter discharge date

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from src.api.doctor import router as doctor_router
 from src.api.nurse import router as nurse_router
 from src.api.cax_codes import router as cax_code_router
 from src.api.medical_history import router as medical_history_router
+from src.api.discharge_date import router as discharge_date_router
 
 app = FastAPI(title="Excel-Worker")
 
@@ -22,3 +23,4 @@ app.include_router(doctor_router)
 app.include_router(nurse_router)
 app.include_router(cax_code_router)
 app.include_router(medical_history_router)
+app.include_router(discharge_date_router)
