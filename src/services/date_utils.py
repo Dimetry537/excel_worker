60eec3354532acad57e3919a0b4c6f9866e1c3e0
Dimetry537 <maksakov1988@gmail.com>
2025-08-08 09:51:27 +0300

    correct date utils method

--- src/services/date_utils.py
+++ src/services/date_utils.py
@@ -4,9 +4,11 @@ import holidays
 RU_HOLIDAYS = holidays.RU()
 
 def calculate_discharge_date(admission_date: date, quantity_of_days: int) -> date:
-    target_date = admission_date + timedelta(days=quantity_of_days)
+    max_date = admission_date + timedelta(days=quantity_of_days)
 
-    while target_date.weekday() >= 5 or target_date in RU_HOLIDAYS:
-        target_date += timedelta(days=1)
+    discharge_date = max_date
 
-    return target_date
+    while discharge_date.weekday() >= 5 or discharge_date in RU_HOLIDAYS:
+        discharge_date -= timedelta(days=1)
+
+    return discharge_date
